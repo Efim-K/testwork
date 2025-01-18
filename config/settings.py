b3e1fce5b3ce789f05a99c737d1b74993bf53324
Efim Kashapov <bambalabes@gmail.com>
2025-01-19 00:19:18 +0500

    Добавлен активный user для использования CRUD

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from pathlib import Path
 
@@ -22,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'network',
     'users',
 ]
@@ -99,3 +101,17 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR.joinpath("media")
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
+}
--- network/views.py
+++ network/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, filters
 
 from network.models import SalesNetwork
 from network.serializers import SalesNetworkSerializer
+from users.permissions import IsActive
 
 
 class SalesNetworkListApiView(generics.ListAPIView):
@@ -11,27 +12,32 @@ class SalesNetworkListApiView(generics.ListAPIView):
     serializer_class = SalesNetworkSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['country']
+    permission_classes = (IsActive,)
 
 
 class SalesNetworkCreateApiView(generics.CreateAPIView):
     """Создание нового участника сети продаж"""
     queryset = SalesNetwork.objects.all()
     serializer_class = SalesNetworkSerializer
+    permission_classes = (IsActive,)
 
 
 class SalesNetworkRetrieveApiView(generics.RetrieveAPIView):
     """Просмотр информации о конкретном участнике сети продаж"""
     queryset = SalesNetwork.objects.all()
     serializer_class = SalesNetworkSerializer
+    permission_classes = (IsActive,)
 
 
 class SalesNetworkUpdateApiView(generics.UpdateAPIView):
     """Изменение информации о конкретном участнике сети продаж"""
     queryset = SalesNetwork.objects.all()
     serializer_class = SalesNetworkSerializer
+    permission_classes = (IsActive,)
 
 
 class SalesNetworkDestroyApiView(generics.DestroyAPIView):
     """Удаление участника сети продаж"""
     queryset = SalesNetwork.objects.all()
     serializer_class = SalesNetworkSerializer
+    permission_classes = (IsActive,)
