368bf645c957787a242db41c4b4cc039c60e7d32
Efim Kashapov <bambalabes@gmail.com>
2025-01-19 01:27:21 +0500

    Добавлена валидация иерархии в админке

--- network/admin.py
+++ network/admin.py
@@ -1,16 +1,39 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from django.urls import reverse
 from network.models import SalesNetwork, Product
+from django.forms import ModelForm
+from django.core.exceptions import ValidationError
+
+class SalesNetworkForm(ModelForm):
+    """Форма для создания/редактирования SalesNetwork"""
+    class Meta:
+        model = SalesNetwork
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        network_type = cleaned_data.get("network_type")
+        supplier = cleaned_data.get("supplier")
+        # Валидация иерархии типа завода с поставщиком
+        if network_type == "FA" and supplier.network_type in ["NW", "IE"]:
+            raise ValidationError(f"У завода не может быть поставщик, кроме другого завода ")
+        # Валидация иерархии типа розничной сети с поставщиком
+        if network_type == "NW" and supplier.network_type in ["IE"]:
+            raise ValidationError("У розничной сети не может быть поставщик индивидуальный предприниматель")
+
+        return cleaned_data
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """Отображение модели Product в админке"""
     list_display = ('id', 'name', 'model', 'release_date',)
 
 
 @admin.register(SalesNetwork)
 class SalesNetworkAdmin(admin.ModelAdmin):
+    """Отображение модели SalesNetwork в админке"""
+    form = SalesNetworkForm
     list_display = (
         'id', 'name', 'network_type', 'email', 'country', 'city', 'street', 'house_number', 'get_supplier_link',
         'get_products', 'debt', 'created_at')
